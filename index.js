9da8ec3f704830b75a8342a48e6a8e48220220d8
Denys web2025-3 <denysoch@email.com>
2025-03-17 22:37:16 +0200

    fix file index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const { program } = require('commander');
 const fs = require('fs');
 
 program
-    .requiredOption('-i, --input <path>', 'шлях до JSON-файлу')
-    .option('-o, --output <path>', 'шлях до файлу для збереження результату')
+    .option('-i, --input <path>', 'шлях до JSON')
+  .option('-o, --output <path>', 'зберегти результат у файл')
     .option('-d, --display', 'вивести результат у консоль');
 
 program.parse(process.argv);
@@ -13,8 +13,13 @@ const inputPath = options.input;
 const outputPath = options.output;
 const displayResult = options.display;
 
+if (!inputPath) {
+    console.error("Please, specify input file");
+    process.exit(1);
+}
+
 if (!fs.existsSync(inputPath)) {
-    console.error('Cannot find input file');
+    console.error("Cannot find input file");
     process.exit(1);
 }
 
@@ -24,7 +29,7 @@ const jsonData = JSON.parse(rawData);
 const filteredData = jsonData.filter(item => item.parent === "BS3_BanksLiab");
 
 const result = filteredData
-    .map(item => `${item.txten}: ${item.value}`) 
+    .map(item => `${item.txten}: ${item.value}`)
     .join('\n');
 
 if (displayResult) {
@@ -35,6 +40,4 @@ if (outputPath) {
     fs.writeFileSync(outputPath, result);
 }
 
-if (!displayResult && !outputPath) {
-    console.log("Please specify output method (-o or -d).");
-}
+
